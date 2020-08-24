18b9d3d6b11841cb540adf5755eb72419fe56872
Black-Tomcat <tomcat.frantz@gmail.com>
2020-08-24 11:35:02 +1000

    Added in CORS header. Shouldn't be causing issues though.

--- frontend/src/app/axiosAccess.ts
+++ frontend/src/app/axiosAccess.ts
@@ -16,7 +16,9 @@ axios.interceptors.request.use(
             config.headers["Authorization"] = `Bearer ${token}`;
             console.log("Setting auth with token");
         }
+
         config.headers["Content-Type"] = "application/json";
+        config.headers["Access-Control-Allow-Origin"] = "*";
         return config;
     },
     (error) => {
