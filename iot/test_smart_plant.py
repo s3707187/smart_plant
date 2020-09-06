82f58d13a92a55898816b09bc15c1ae4c33ca469
s3707187 <s3707187@student.rmit.edu.au>
2020-09-07 06:29:29 +1000

    updated story 18 tests for post

--- iot/test_smart_plant.py
+++ iot/test_smart_plant.py
@@ -14,7 +14,7 @@ class MockResponse():
     def __init__(self, code):
         self.status_code = code
 
-def mock_get(url, json=None):
+def mock_post(url, json=None):
     if url == "{}/verify_plant".format(smart_plant.API_URL):
         if json["plant_id"] == "01" and json["password"] == "testpassword":
             return MockResponse(200)
@@ -45,22 +45,22 @@ class MainSystemTest(unittest.TestCase):
             os.remove(TEST_CONFIG)
 
 
-    @mock.patch('requests.get', side_effect=mock_get)
+    @mock.patch('requests.post', side_effect=mock_post)
     def test_verify_success(self, mock_requests):
         verified = self.sr.verify_plant("01", "testpassword")
         self.assertTrue(verified)
 
-    @mock.patch('requests.get', side_effect=mock_get)
+    @mock.patch('requests.post', side_effect=mock_post)
     def test_verify_bad_id(self, mock_requests):
         verified = self.sr.verify_plant("00", "testpassword")
         self.assertFalse(verified)
 
-    @mock.patch('requests.get', side_effect=mock_get)
+    @mock.patch('requests.post', side_effect=mock_post)
     def test_verify_bad_pass(self, mock_requests):
         verified = self.sr.verify_plant("01", "badpassword")
         self.assertFalse(verified)
     
-    @mock.patch('requests.get', side_effect=mock_get)
+    @mock.patch('requests.post', side_effect=mock_post)
     def test_verify_bad_both(self, mock_requests):
         verified = self.sr.verify_plant("00", "badpassword")
         self.assertFalse(verified)
