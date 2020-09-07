c23e0746cfefcce4d977187925bb6f80ef963780
s3707187 <s3707187@student.rmit.edu.au>
2020-09-08 03:33:07 +1000

    commenting code

--- iot/smart_plant.py
+++ iot/smart_plant.py
@@ -13,7 +13,6 @@ from sensors import SensorManager
 
 CURR_DIR = os.path.dirname(__file__)
 
-# SM = SensorManager()
 
 LOG_FILE_PATH = os.path.join(CURR_DIR, "log.csv")
 LOG_ENABLED = False
@@ -25,14 +24,16 @@ API_URL = "http://127.0.0.1:8080"
 CONFIG_FILE_NAME = "cloud_config.json"
 CONFIG_FILE_PATH = os.path.join(CURR_DIR, CONFIG_FILE_NAME)
 
+
 class SystemRunner:
     def __init__(self):
+        # sensor manager used to get all sensor data
         self.SM = SensorManager()
 
     def menu_system(self):
-        
-
+        # main menu and user interaction
         while True:
+            # print menu
             print()
             print("Welcome to your Smart Plant. Please select an option.")
             print("1. Begin local data collection.")
@@ -41,82 +42,93 @@ class SystemRunner:
             # print("2. Calibrate moisture sensor.") - would be an extra feature
             select = input("Selection: ")
 
+            # local collection
             if select == "1":
                 period = input("Please type a sample rate (in seconds): ")
                 try:
                     period_t = float(period)
                     self.start_sampling(period_t)
-
+                # notify of error
                 except ValueError:
                     print("Invalid number. Try again.")
+            # cloud config
             elif select == "2":
                 self.configure_cloud()
+            # exit
             elif select == "3":
                 self.clean_exit()
             else:
+                # retry
                 print("Invalid selection. Try again.")
 
-
     def start_sampling(self, period_t, logging=False):
+        # method to start local logging of data
+        # period_t parameter defines time between data samples/logs
+        # logging parameter set to True if file logging is desired
         while True:
-
+            # get sensor data
             light = round(self.SM.get_light_pct(), 2)
             humid = round(self.SM.get_humidity_pct(), 2)
             temp = round(self.SM.get_temp_val(), 2)
             moist = round(self.SM.get_moisture_pct(), 2)
-
+            # log to file if enabled
             if(logging):
+                # log with datetime
                 curr_time = datetime.datetime.now().strftime("%H:%M:%S "
-                                                            "%Y-%m-%d")
+                                                             "%Y-%m-%d")
                 curr_log_file = open(LOG_FILE_PATH, 'a')
+                # write in comma separated columns
                 curr_log_file.write('{},{},{},{},{},\n'.format(
                     curr_time, light, humid, temp, moist))
                 curr_log_file.close()
             else:
+                # print data to console if logging disabled
                 print("Light %: {}".format(light))
                 print("Humidity %: {}".format(humid))
                 print("Moisture %: {}".format(moist))
                 print("Temperature (Celsius): {}".format(temp))
-
+            # sleep for period
             time.sleep(period_t)
 
-
     def configure_cloud(self):
-        # connect the plant with the cloud for the first time *
-        # get user input *
-        # create the config JSON file, put JSON config information *
+        # this method is used to connect the plant with the cloud.
+        # config is based on user input.
+        # configuration JSON file is created.
         # if APi check fails, log error output - actually probably not since we have interactivity here
         again = True
         while again:
+            # print info
             print("Welcome to the cloud configuration center.\n")
             print("Here you can enter your plant ID and activation key to link it to "
-                "the cloud and enable data uploads.")
+                  "the cloud and enable data uploads.")
             print("The activation details for the plants you own are available on the web "
-                "application")
-
+                  "application")
+            # get plant ID from user
             entered_id = input(
                 "Please enter the plant ID (enter nothing to cancel): ")
             if entered_id != "":
+                # get plant key/password from user
                 entered_key = input(
                     "Please enter the plant activation key (enter nothing to cancel): ")
                 if entered_key != "":
-                    # may hash entered key here
                     hasher = hashlib.sha256()
                     hasher.update(bytes(entered_key, 'utf-8'))
-                    # Uncomment this line and comment the following if want hashing
+                    # Uncomment this line and comment the one after if want hashing
                     # hashed_key = hasher.hexdigest()
                     hashed_key = entered_key
-
+                    # verify entered details
                     verified = self.verify_plant(entered_id, hashed_key)
 
                     if verified:
+                        # save details to file if valid, exit cloud center
                         json_info = {"plant_id": entered_id,
-                                    "plant_key": hashed_key}
+                                     "plant_key": hashed_key}
                         with open(CONFIG_FILE_PATH, 'w+') as config_file:
                             json.dump(json_info, config_file)
                         print("Successful cloud link! \n")
                         again = False
                     else:
+                        # option to try again if verification failed (invalid details)
                         print("Error: plant ID or activation key is incorrect.")
                         user_choice = input("Try again? (y/n): ")
                         if user_choice.lower() == "y":
@@ -124,58 +136,60 @@ class SystemRunner:
                         else:
                             again = False
                 else:
+                    # entering nothing exits the cloud center
                     again = False
             else:
+                # entering nothing exits the cloud center
                 again = False
-
 
     def verify_plant(self, plant_id, key):
         # returns true or false, calls API on plant check
-
+        # details JSON
         details = {"plant_id": plant_id,
-                "password": key
-                }
+                   "password": key
+                   }
         try:
-            response = requests.post("{}/verify_plant".format(API_URL), json=details)
-        except (ConnectionRefusedError, 
-                urllib3.exceptions.NewConnectionError, 
+            # post details to API, get response
+            response = requests.post(
+                "{}/verify_plant".format(API_URL), json=details)
+        except (ConnectionRefusedError,
+                urllib3.exceptions.NewConnectionError,
                 urllib3.exceptions.MaxRetryError,
                 requests.exceptions.ConnectionError) as e:
+            # catch all connection exceptions, log an error
             self.log_error("Error accessing API: {}".format(e))
+            # return False for failure
             return False
 
+        # only return True if status is good (200)
         if response.status_code == 200:
             return True
         else:
             return False
 
-        # return True
-
-
     def upload_data(self, plant_id, plant_key, date_time, light, moisture, humidity, temperature):
-        # uploads a single peice of data, should only be called if
-        # plant configuration is verified
-
+        # uploads a single piece of data
+        # put information into JSON
         information = {"date_time": date_time,
-                    "light": light,
-                    "moisture": moisture,
-                    "humidity": humidity,
-                    "temperature": temperature,
-                    "plant_id": plant_id,
-                    "password": plant_key
-                    }
+                       "light": light,
+                       "moisture": moisture,
+                       "humidity": humidity,
+                       "temperature": temperature,
+                       "plant_id": plant_id,
+                       "password": plant_key
+                       }
+        # post data and get response
         response = requests.post(
             "{}/save_plant_data".format(API_URL), json=information)
         # response code 201 is "Created"
         if response.status_code != 201:
+            # if bad code, log error
             self.log_error("Data upload failed. Response code {}".format(
                 response.status_code))
 
-
     def start_uploading(self, period_t, max_uploads=-1):
-        # similar to start_sampling but uploads to cloud instead
+        # similar to start_sampling but uploads to cloud instead.
         # verifies plant first, if fails then log error output and exits
-        # may throw exceptions from sensors, catch here
 
         # first get details from file
         credentials = None
@@ -183,59 +197,74 @@ class SystemRunner:
             with open(CONFIG_FILE_PATH, "r") as config_file:
                 credentials = json.load(config_file)
         except FileNotFoundError:
+            # log error if credentials not found
             error_message = "Credentials file not found."
             self.log_error(error_message)
         except json.JSONDecodeError:
             error_message = "Credentials file not valid JSON."
+            # log error if credentials are found but not loadable as JSON
             self.log_error(error_message)
         if credentials is not None:
+            # verify credentials
             if self.verify_plant(credentials["plant_id"], credentials["plant_key"]):
                 upload_count = 0
+                # by default, uploads until interrupted.
+                # otherwise uploads until argument of max_uploads reached
                 while upload_count < max_uploads:
                     curr_time = datetime.datetime.now().strftime("%H:%M:%S "
-                                                                "%Y-%m-%d")
+                                                                 "%Y-%m-%d")
                     # may surround next block in try catch, catching sensor exceptions
-                    # and logging them but continuing without uploading
+                    # and logging them but continuing without uploading.
+                    # sensor exceptions may not be necessary
                     light = round(self.SM.get_light_pct(), 2)
                     moist = round(self.SM.get_moisture_pct(), 2)
                     humid = round(self.SM.get_humidity_pct(), 2)
                     temp = round(self.SM.get_temp_val(), 2)
-
-                    self.upload_data(credentials["plant_id"], credentials["plant_key"], curr_time, light, moist, humid, temp)
+                    # upload data
+                    self.upload_data(
+                        credentials["plant_id"], credentials["plant_key"], curr_time, light, moist, humid, temp)
                     upload_count += 1
+                    # wait
                     time.sleep(period_t)
             else:
                 error_message = "Invalid credentials from file."
+                # log error if credentials were found but are invalid
                 self.log_error(error_message)
-
+        # exit
         self.clean_exit(False)
-
 
     def log_error(self, message):
         # log the datetime+message to error_log.txt
         curr_time = datetime.datetime.now().strftime("%H:%M:%S "
-                                                    "%Y-%m-%d")
+                                                     "%Y-%m-%d")
         with open(ERROR_FILE_PATH, "a+") as error_file:
             error_file.write("{} $ {}\n".format(curr_time, message))
-
 
     def clean_exit(self, notify=True):
         if notify:
             print("\nClosing.")
+        # cleanup sensors via manager
         self.SM.cleanup()
         exit()
 
+
 def boot_config():
+    # quick check to grab a config file from /boot partition.
+    # this function helps users who cannot SSH/access the Pi,
+    # but can access the microSD card
     if os.path.exists("/boot/plant.config"):
         print("Configuration loaded from /boot directory.")
         new_path = os.path.join(CURR_DIR, CONFIG_FILE_NAME)
         copyfile(BOOT_CONFIG_PATH, new_path)
 
+
 if __name__ == '__main__':
+    # check if /boot config file exists
     boot_config()
     runner = SystemRunner()
 
     try:
+        # argument config
         parser = argparse.ArgumentParser()
 
         parser.add_argument("-m", "--menu", action='store_true',
@@ -251,8 +280,9 @@ if __name__ == '__main__':
                             default=False, help="Automatically start uploading to cloud.")
 
         args = vars(parser.parse_args())
-        
+
         # extra check here just so we don't go through cloud billing
+        # 1800 seconds is 30 minutes
         if args["sample_rate"] < 1800:
             args["sample_rate"] = 1800
 
@@ -278,8 +308,10 @@ if __name__ == '__main__':
                     runner.clean_exit()
 
             if args["menu"]:
+                # show the menu
                 runner.menu_system()
             else:
+                # regular auto local logging if menu-less mode
                 runner.start_sampling(args["sample_rate"], LOG_ENABLED)
 
     except KeyboardInterrupt:
--- iot/test_smart_plant.py
+++ iot/test_smart_plant.py
@@ -17,20 +17,26 @@ class MockResponse():
         self.status_code = code
 
 def mock_post(url, json=None):
+    # function to mock post method
     if url == "{}/verify_plant".format(smart_plant.API_URL):
+        # mock plant verification via post
         if json["plant_id"] == "01" and json["password"] == "testpassword":
+            # success
             return MockResponse(200)
         else:
             return MockResponse(401)
 
     elif url == "{}/save_plant_data".format(smart_plant.API_URL):
+        # mock data upload via post
         if json["plant_id"] == "01" and json["password"] == "testpassword":
+            # success
             return MockResponse(201)
         else:
             return MockResponse(401)
     else:
         return MockResponse(404)
 
+# mocks for plant verification method itself
 def mock_verify_plant_success(plant_id, key):
     return True
 
@@ -38,21 +44,29 @@ def mock_verify_plant_fail(plant_id, key):
     return False
 
 def mock_verify_plant_check(plant_id, key):
+    # when the passed ID/key actually matter, use this.
+    # returns true only for this combination
     if plant_id == "01" and key == "testpassword":
         return True
     else:
         return False
 
 class ErrorMock:
+    # class that mocks error logging.
+    # stores previous error
     previous_error = None
     @staticmethod
     def mock_error(message):
+        # update error stored
         ErrorMock.previous_error = message
 
 class DataUploadMock:
+    # class that mocks data uploads.
+    # stores previous upload
     previous_upload = None
     @staticmethod
     def mock_upload(plant_id, plant_key, date_time, light, moisture, humidity, temperature):
+        # update data stored
         DataUploadMock.previous_upload = (plant_id, plant_key, date_time, light, moisture, humidity, temperature)
 
 class MainSystemTest(unittest.TestCase):
@@ -61,29 +75,35 @@ class MainSystemTest(unittest.TestCase):
 
     def tearDown(self):
         self.sr.SM.cleanup()
+        # remove any cloud config test file
         if os.path.exists(TEST_CONFIG):
             os.remove(TEST_CONFIG)
+        # reset mock class storage variables
         ErrorMock.previous_error = None
         DataUploadMock.previous_upload = None
 
 
     @mock.patch('requests.post', side_effect=mock_post)
     def test_verify_success(self, mock_requests):
+        # test plant verification is successful with valid data
         verified = self.sr.verify_plant("01", "testpassword")
         self.assertTrue(verified)
 
     @mock.patch('requests.post', side_effect=mock_post)
     def test_verify_bad_id(self, mock_requests):
+        # test plant verification is unsuccessful with invalid ID
         verified = self.sr.verify_plant("00", "testpassword")
         self.assertFalse(verified)
 
     @mock.patch('requests.post', side_effect=mock_post)
     def test_verify_bad_pass(self, mock_requests):
+        # test plant verification is unsuccessful with invalid pass
         verified = self.sr.verify_plant("01", "badpassword")
         self.assertFalse(verified)
     
     @mock.patch('requests.post', side_effect=mock_post)
     def test_verify_bad_both(self, mock_requests):
+        # test plant verification is unsuccessful with both invalid
         verified = self.sr.verify_plant("00", "badpassword")
         self.assertFalse(verified)
 
@@ -91,16 +111,18 @@ class MainSystemTest(unittest.TestCase):
     @mock.patch('smart_plant.input', create=True)
     @mock.patch('smart_plant.SystemRunner.verify_plant', side_effect=mock_verify_plant_success)
     def test_configure_cloud_success(self, mock_verify, mock_input):
+        # test cloud link is successful with valid details
         mock_input.side_effect = ["01", "testpassword"]
-
+        # capture standard out
         captured_output = io.StringIO()
         sys.stdout = captured_output
 
         self.sr.configure_cloud()
 
+        # reset standard out
         sys.stdout = sys.__stdout__
         output = captured_output.getvalue()
-
+        # check output
         self.assertTrue("Welcome" in output)
         self.assertTrue("Successful cloud link!" in output)
 
@@ -108,16 +130,18 @@ class MainSystemTest(unittest.TestCase):
     @mock.patch('smart_plant.input', create=True)
     @mock.patch('smart_plant.SystemRunner.verify_plant', side_effect=mock_verify_plant_fail)
     def test_configure_cloud_fail(self, mock_verify, mock_input):
+        # test cloud link is unsuccessful with invalid details
         mock_input.side_effect = ["01", "testpassword", "n"]
 
+        # capture standard out
         captured_output = io.StringIO()
         sys.stdout = captured_output
 
         self.sr.configure_cloud()
-
+        # reset standard out
         sys.stdout = sys.__stdout__
         output = captured_output.getvalue()
-
+        # check output
         self.assertTrue("Welcome" in output)
         self.assertTrue("Error: plant ID or activation key is incorrect." in output)
 
@@ -125,16 +149,18 @@ class MainSystemTest(unittest.TestCase):
     @mock.patch('smart_plant.input', create=True)
     @mock.patch('smart_plant.SystemRunner.verify_plant', side_effect=mock_verify_plant_check)
     def test_configure_cloud_repeat(self, mock_verify, mock_input):
+        # test successful cloud configuration after multiple failed attempts
         mock_input.side_effect = ["01", "badpassword", "y", "22", "badpassword2", "y", "01", "testpassword"]
 
+        # capture standard out
         captured_output = io.StringIO()
         sys.stdout = captured_output
 
         self.sr.configure_cloud()
-
+        # reset standard out
         sys.stdout = sys.__stdout__
         output = captured_output.getvalue()
-
+        # check output contains expected number of errors and success message
         self.assertEqual(output.count("Welcome"), 3)
         self.assertEqual(output.count("Error: plant ID or activation key is incorrect."), 2)
         self.assertTrue("Successful cloud link!" in output)
@@ -143,19 +169,23 @@ class MainSystemTest(unittest.TestCase):
     @mock.patch('smart_plant.input', create=True)
     @mock.patch('smart_plant.SystemRunner.verify_plant', side_effect=mock_verify_plant_success)
     def test_configure_cloud_file_check(self, mock_verify, mock_input):
+        # test cloud link is successful with valid details.
+        # especially check new file contents
         mock_input.side_effect = ["01", "testpassword"]
 
+        # capture standard out
         captured_output = io.StringIO()
         sys.stdout = captured_output
 
         self.sr.configure_cloud()
-
+        # reset standard out
         sys.stdout = sys.__stdout__
         output = captured_output.getvalue()
-
+        # check file exists
         self.assertTrue(os.path.exists(TEST_CONFIG))
         file_check = False
         with open(TEST_CONFIG, "r") as test_config_file:
+            # check file contents are correct
             file_check = '{"plant_id": "01", "plant_key": "testpassword"}' in test_config_file.read()
         self.assertTrue(file_check)
         
@@ -165,12 +195,14 @@ class MainSystemTest(unittest.TestCase):
     @mock.patch('requests.post', side_effect=mock_post)
     @mock.patch('smart_plant.SystemRunner.log_error', side_effect=ErrorMock.mock_error)
     def test_upload_data(self, mock_error_log, mock_requests):
+        # test data uploads with no error (mocked API)
         self.sr.upload_data("01", "testpassword", "01:49:12 2020-08-24", 35, 50, 35, 21)
         self.assertIsNone(ErrorMock.previous_error)
 
     @mock.patch('requests.post', side_effect=mock_post)
     @mock.patch('smart_plant.SystemRunner.log_error', side_effect=ErrorMock.mock_error)
     def test_upload_data_invalid(self, mock_error_log, mock_requests):
+        # test data uploads with error (mocked API) due to invalid details (ID = 02 instead of 01)
         self.sr.upload_data("02", "testpassword", "01:49:12 2020-08-24", 35, 50, 35, 21)
         self.assertTrue("Data upload failed. Response code" in ErrorMock.previous_error)
 
@@ -180,9 +212,12 @@ class MainSystemTest(unittest.TestCase):
     @mock.patch('smart_plant.SystemRunner.clean_exit', side_effect=None)
     @mock.patch('smart_plant.SystemRunner.log_error', side_effect=ErrorMock.mock_error)
     def test_start_uploading(self, mock_error_log, mock_exit_clean, mock_data_up, mock_verification):
+        # test auto cloud uploading (mocked API), with one repeat
         self.sr.start_uploading(1, 1)
+        # ensure no error logged
         self.assertTrue(ErrorMock.previous_error is None)
         for i in range(0,7):
+            # ensure each part of previous upload exists
             self.assertTrue(DataUploadMock.previous_upload[i] is not None)
 
     @mock.patch('smart_plant.SystemRunner.verify_plant', side_effect=mock_verify_plant_success)
@@ -191,7 +226,9 @@ class MainSystemTest(unittest.TestCase):
     @mock.patch('smart_plant.SystemRunner.clean_exit', side_effect=None)
     @mock.patch('smart_plant.SystemRunner.log_error', side_effect=ErrorMock.mock_error)
     def test_start_uploading_no_creds(self, mock_error_log, mock_exit_clean, mock_data_up, mock_verification):
+        # test failed auto cloud uploading (mocked API), with one repeat
         self.sr.start_uploading(1, 1)
+        # credentials file does not exist
         self.assertTrue("Credentials file not found." in ErrorMock.previous_error)
         self.assertTrue(DataUploadMock.previous_upload is None)
 
@@ -201,7 +238,9 @@ class MainSystemTest(unittest.TestCase):
     @mock.patch('smart_plant.SystemRunner.clean_exit', side_effect=None)
     @mock.patch('smart_plant.SystemRunner.log_error', side_effect=ErrorMock.mock_error)
     def test_start_uploading_bad_creds(self, mock_error_log, mock_exit_clean, mock_data_up, mock_verification):
+        # test failed auto cloud uploading (mocked API), with one repeat
         self.sr.start_uploading(1, 1)
+        # credentials file is of bad format
         self.assertTrue("Credentials file not valid JSON." in ErrorMock.previous_error)
         self.assertTrue(DataUploadMock.previous_upload is None)
 
