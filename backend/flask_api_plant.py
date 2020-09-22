2caf984fa12aec681ff3de8288ad7c27c05f33d5
s3707187 <s3707187@student.rmit.edu.au>
2020-09-22 22:18:58 +1000

    forgot to save access changes

--- backend/flask_api_plant.py
+++ backend/flask_api_plant.py
@@ -163,9 +163,9 @@ def view_plant_details():
         
         if not get_plant_edit_permission(current_user, plant_id):
             plant_info["password"] = None
-            plant_info["access"] = "plant_viewer"
+            plant_info["access"] = "read"
         else:
-            plant_info["access"] = "plant_owner"
+            plant_info["access"] = "edit"
         return jsonify(plant_info)
 
     else:
