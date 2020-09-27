383ea803a47195024da23fdc044736eedf3ca26e
s3707187 <s3707187@student.rmit.edu.au>
2020-09-28 04:44:15 +1000

    get plants tested

--- backend/backend_testing/test_api_plant.py
+++ backend/backend_testing/test_api_plant.py
@@ -1,25 +1,48 @@
 from backend_testing.setup_tests import *
-
 from flask_jwt_extended import create_access_token
+from main import app
 
-TEST_USER_1_TOKEN = create_access_token('test_01', user_claims={"role": 'user'})
-TEST_USER_2_TOKEN = create_access_token('test_02', user_claims={"role": 'user'})
-TEST_ADMIN_TOKEN = create_access_token('test_admin', user_claims={"role": 'admin'})
+TEST_USER_1 = 'test_01'
+TEST_USER_2 = 'test_02'
+TEST_ADMIN = 'test_admin'
 
-def get_auth_header(token):
+NUM_TEST_PLANTS = 2
+
+def get_auth_header(client, identity, role):
+    # need to temporarily simulate the request context inorder to create the
+    # access token
+    with app.test_request_context():
+        token = create_access_token(identity, user_claims={"role": role})
+    # make the header and return
     auth_header = {
         'Authorization': 'Bearer {}'.format(token)
     }
     return auth_header
 
-# DEMONSTRATION METHOD, DELETE TODO
-def test_get_plants(client):
-    response = client.get('/plants')
-    print(response)
 
-def test_get_all_plants_fail(client):
-    header = get_auth_header(TEST_USER_1_TOKEN)
-    response = client.get('')
+def test_user_plants(client):
+    header = get_auth_header(client, TEST_USER_1, 'user')
+    response = client.get('/get_users_plants', headers=header)
+    assert response.status_code == 200
+    assert len(response.json) >= 1
+    for plant in response.json:
+        assert plant['plant_id'] is not None
+        assert plant['plant_name'] is not None
+        assert plant['password'] is not None
+        assert plant['plant_type'] is not None
+
+
+def test_admin_plants(client):
+    header = get_auth_header(client, TEST_ADMIN, 'admin')
+    response = client.get('/get_users_plants', headers=header)
+    assert response.status_code == 200
+    assert len(response.json) >= NUM_TEST_PLANTS
+    for plant in response.json:
+        assert plant['plant_id'] is not None
+        assert plant['plant_name'] is not None
+        assert plant['password'] is not None
+        assert plant['plant_type'] is not None
+
 
 # get all plants
 # try with not admin
--- backend/backend_testing/test_api_user.py
+++ backend/backend_testing/test_api_user.py
@@ -2,9 +2,9 @@ from backend_testing.setup_tests import *
     
 
 # DEMONSTRATION METHOD, DELETE TODO
-def test_get_plants(client):
-    response = client.get('/plants')
-    print(response)
+# def test_get_plants(client):
+#     response = client.get('/plants')
+#     print(response)
 
 # login admin
 # try login with bad password
