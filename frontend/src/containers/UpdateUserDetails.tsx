6a979a0f2de5f975e2514fedf6648d4674fdc486
Black-Tomcat <tomcat.frantz@gmail.com>
2020-09-30 00:39:43 +1000

    Bugfix the input for a plant modal.

---
+++ frontend/src/containers/UpdateUserDetails.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+interface UpdateUserDetailsProps {
+}
+
+const UpdateUserDetails: React.FC<UpdateUserDetailsProps> = (props: UpdateUserDetailsProps) => {
+    return null
+};
+
+export default UpdateUserDetails;
