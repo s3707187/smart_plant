bf2673c51760e2a1368e7329d6d12e7e49c93a4c
Black-Tomcat <tomcat.frantz@gmail.com>
2020-10-05 17:50:28 +1100

    Added functionality to the user dashboard section (only happy paths)

--- frontend/src/screens/UserDashboardScreen.tsx
+++ frontend/src/screens/UserDashboardScreen.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
-import { Button, Layout, Spin, Typography, Table, Space } from "antd";
+import { Button, Layout, Spin, Typography, Table, Space, Modal, Form, message } from "antd";
 import { SettingOutlined } from "@ant-design/icons";
+import AddUserForm from "../containers/AddUserForm";
+import UpdateUserDetailsModal from "../containers/UpdateUserDetailsModal";
 import { useGet, usePost } from "../utils/apiHooks";
 
 const { Header, Content } = Layout;
@@ -10,14 +12,72 @@ const { Column } = Table;
 interface UserDashboardScreenProps {}
 
 const UserDashboardScreen: React.FC<UserDashboardScreenProps> = (props: UserDashboardScreenProps) => {
-    const [visible, setVisible] = useState(false);
+    const [addUserVisible, setAddUserVisible] = useState(false);
+    const [editUserVisible, setEditUserVisible] = useState(false);
+    const [selectedUser, setSelectedUser] = useState<
+        undefined | { first_name: string; last_name: string; email: string; username: string }
+    >(undefined);
+    const [form] = Form.useForm();
 
     const { data, errors, loading, refetch } = useGet("all_users");
     // console.log("DATA", data);
     const [DeleteUser] = usePost<{}, { user_to_del: string }>("delete_user");
+    const [Register] = usePost<
+        { refresh_token: string; access_token: string },
+        {
+            username: string;
+            password: string;
+            first_name: string;
+            last_name: string;
+            email: string;
+        }
+    >("register", form);
+
+    const onFinish = async (values: any) => {
+        await Register({
+            username: values.username,
+            password: values.password,
+            first_name: values.first_name,
+            last_name: values.last_name,
+            email: values.email,
+        });
+
+        message.success(`${values.username} registered!`);
+
+        setAddUserVisible(false);
+        form.resetFields();
+        refetch();
+    };
 
     return (
         <Layout style={{ flexGrow: 1 }}>
+            <Modal
+                visible={addUserVisible}
+                onOk={() => {
+                    form.submit();
+                }}
+                onCancel={() => {
+                    setAddUserVisible(false);
+                }}
+                title={"Add New User"}
+            >
+                <AddUserForm onFinish={onFinish} form={form} />
+            </Modal>
+            {selectedUser !== undefined && (
+                <UpdateUserDetailsModal
+                    visible={editUserVisible}
+                    onCancel={() => {
+                        setEditUserVisible(false);
+                        setSelectedUser(undefined);
+                    }}
+                    onOk={() => {
+                        setSelectedUser(undefined);
+                        refetch();
+                    }}
+                    username={selectedUser.username}
+                    initialValues={selectedUser}
+                />
+            )}
             <Header
                 style={{
                     background: "#FFF",
@@ -30,7 +90,7 @@ const UserDashboardScreen: React.FC<UserDashboardScreenProps> = (props: UserDash
                     Admin User Dashboard
                 </Title>
                 <div style={{ display: "flex", alignItems: "center" }}>
-                    <Button type={"primary"} onClick={() => setVisible(true)}>
+                    <Button type={"primary"} onClick={() => setAddUserVisible(true)}>
                         Add User
                     </Button>
                 </div>
@@ -49,9 +109,26 @@ const UserDashboardScreen: React.FC<UserDashboardScreenProps> = (props: UserDash
                     <Column
                         title="Action"
                         key="action"
-                        render={(text, record: { username: string }) => (
+                        render={(
+                            text,
+                            record: {
+                                first_name: string;
+                                last_name: string;
+                                email: string;
+                                username: string;
+                                password: string;
+                            }
+                        ) => (
                             <Space size="middle">
-                                <a onClick={() => {}}>Edit</a>
+                                <a
+                                    onClick={() => {
+                                        const { password, ...record_new } = record;
+                                        setSelectedUser(record_new);
+                                        setEditUserVisible(true);
+                                    }}
+                                >
+                                    Edit
+                                </a>
                                 <a
                                     onClick={async () => {
                                         await DeleteUser({ user_to_del: record.username });
