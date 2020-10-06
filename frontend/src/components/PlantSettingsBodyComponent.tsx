60e6b8b1bb327b0c813d9f6e63e8baa2c87e3600
Black-Tomcat <tomcat.frantz@gmail.com>
2020-10-06 23:32:19 +1100

    Finished components with admin allocation and de-allocation

--- frontend/src/components/PlantSettingsBodyComponent.tsx
+++ frontend/src/components/PlantSettingsBodyComponent.tsx
@@ -1,21 +1,25 @@
 import React, { useContext } from "react";
 import { Typography, Popconfirm } from "antd";
 import { useHistory } from "react-router-dom";
-import AuthContex, { getRole } from "../contexts/AuthContex";
+import AuthContex, { getRole, getUserID } from "../contexts/AuthContex";
 import { usePost } from "../utils/apiHooks";
 
 const { Link } = Typography;
 
 interface PlantSettingsBodyComponentProps {
     plantID: number;
+    maintainer?: string;
+    topLoading?: boolean;
+    refetch(): void;
 }
 
 const PlantSettingsBodyComponent: React.FC<PlantSettingsBodyComponentProps> = (
     props: PlantSettingsBodyComponentProps
 ) => {
     const history = useHistory();
-    const { plantID: plant_id } = props;
+    const { maintainer, plantID: plant_id, refetch, topLoading } = props;
     const { token } = useContext(AuthContex);
+    const current_user = token && getUserID(token);
 
     const [DeletePlant] = usePost<{}, { plant_id: number }>("delete_plant");
 
@@ -23,6 +27,33 @@ const PlantSettingsBodyComponent: React.FC<PlantSettingsBodyComponentProps> = (
         await DeletePlant({ plant_id }).then(() => {
             history.push("/");
         });
+    };
+
+    const [RemovePlantMaintainer, { loading: removeLoading }] = usePost<
+        {},
+        { linked_user: string; plant_id: number; link_type: "plant_viewer" | "maintenance" }
+    >("remove_plant_link");
+
+    const [AddPlantMaintainer, { loading: addLoading }] = usePost<
+        {},
+        { user_to_link: string; user_link_type: string; plant_id: number }
+    >("add_plant_link");
+
+    const toggleMaintenance = async () => {
+        if (token && maintainer == undefined)
+            await AddPlantMaintainer({
+                user_to_link: getUserID(token),
+                user_link_type: "maintenance",
+                plant_id,
+            });
+        else if (maintainer != undefined) {
+            await RemovePlantMaintainer({
+                linked_user: maintainer,
+                link_type: "maintenance",
+                plant_id,
+            });
+        }
+        refetch();
     };
 
     return (
@@ -36,7 +67,14 @@ const PlantSettingsBodyComponent: React.FC<PlantSettingsBodyComponentProps> = (
             >
                 <Link type={"danger"}>Delete Plant</Link>
             </Popconfirm>
-            {token && getRole(token) === "admin" && <Link>Assign yourself to plant maintenance.</Link>}
+            {token && getRole(token) === "admin" && (
+                <Link disabled={addLoading || removeLoading || topLoading} onClick={toggleMaintenance}>
+                    {maintainer != undefined && maintainer === current_user
+                        ? "Deassign admin from "
+                        : "Assign yourself to "}
+                    plant maintenance.
+                </Link>
+            )}
         </div>
     );
 };
